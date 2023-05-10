2e03af8cce12fd45ccae7573da10816c392a6f2b
Lucas Takamoto <lucas.takamoto@hotmail.com>
2023-05-09 22:58:53 -0300

    Projeto criado

---
+++ src/main/java/com/lucastakamoto/lucaslist/LucaslistApplication.java
@@ -0,0 +1,13 @@
+package com.lucastakamoto.lucaslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucaslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucaslistApplication.class, args);
+	}
+
+}
