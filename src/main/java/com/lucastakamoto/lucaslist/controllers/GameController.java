ae1f7c8305c113f496406950c7ceed887f4185f2
Lucas Takamoto <lucas.takamoto@hotmail.com>
2023-05-10 22:38:31 -0300

    GET game by id

--- src/main/java/com/lucastakamoto/lucaslist/controllers/GameController.java
+++ src/main/java/com/lucastakamoto/lucaslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lucastakamoto.lucaslist.dto.GameDTO;
 import com.lucastakamoto.lucaslist.dto.GameMinDTO;
 import com.lucastakamoto.lucaslist.services.GameService;
 
@@ -16,6 +18,12 @@ public class GameController {
 
 	@Autowired
 	private GameService gameService;
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		GameDTO result = gameService.findBtId(id);
+		return result;
+	}
 	
 	@GetMapping
 	public List<GameMinDTO> findAll(){
--- src/main/java/com/lucastakamoto/lucaslist/services/GameService.java
+++ src/main/java/com/lucastakamoto/lucaslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.lucastakamoto.lucaslist.dto.GameDTO;
 import com.lucastakamoto.lucaslist.dto.GameMinDTO;
 import com.lucastakamoto.lucaslist.entities.Game;
 import com.lucastakamoto.lucaslist.repositories.GameRepository;
@@ -15,6 +17,14 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 
+	@Transactional(readOnly = true)
+	public GameDTO findBtId(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	
+	
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
